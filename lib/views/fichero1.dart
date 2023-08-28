11717c53c15518ba2d1f926f0acde35a650ec24b
devgamez2022 <101201095+devgamez2022@users.noreply.github.com>
2023-08-27 20:11:12 -0600

    Quinto commit contenido fichero 1

---
+++ lib/views/fichero1.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class Fichero1 extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Fichero One'),
+      ),
+      body: Column(
+        children: [
+
+        ],
+      ),
+    );
+  }
+
+}
